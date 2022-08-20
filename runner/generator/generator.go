08d9bf9ca286a6917b2db0900aaa22d6005560b9
tady <ohtukayoshi.yoshi@gmail.com>
2022-08-20 13:15:04 +0900

    add generator

--- runner/generator/generator.go
+++ runner/generator/generator.go
@@ -1,17 +1,53 @@
 package generator
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
-func MakeDir(name string) error {
-	dp := filepath.Join(name)
-	if err := os.Mkdir(dp, 0700); err != nil {
+func MakeRoot(name string) error {
+	if err := os.MkdirAll(name, 0700); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func copyFile() {}
+func Scan(src, dst string) error {
+	fs, err := ioutil.ReadDir(src)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range fs {
+		if f.IsDir() {
+			if err := os.MkdirAll(filepath.Join(dst, f.Name()), 0700); err != nil {
+				return err
+			}
+			subDirSrc, subDirDst := filepath.Join(src, f.Name()), filepath.Join(dst, f.Name())
+			if err := Scan(subDirSrc, subDirDst); err != nil {
+				return err
+			}
+		} else {
+			fileSrc := filepath.Join(src, f.Name())
+			buf, err := ioutil.ReadFile(fileSrc)
+			if err != nil {
+				return err
+			}
+
+			fileDst := filepath.Join(dst, f.Name())
+			file, err := os.Create(fileDst)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			if _, err = file.Write(buf); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
--- runner/sandbox/sandbox.go
+++ runner/sandbox/sandbox.go
@@ -1,13 +1,22 @@
 package sandbox
 
 import (
+	"fmt"
+
 	"github.com/TadayoshiOtsuka/go-tady/runner/config"
 	"github.com/TadayoshiOtsuka/go-tady/runner/generator"
 )
 
+const src = "./templates/sandbox"
+
 func Create() error {
 	projectName := config.Config.Name
-	if err := generator.MakeDir(projectName); err != nil {
+	if err := generator.MakeRoot(projectName); err != nil {
+		return err
+	}
+
+	if err := generator.Scan(src, projectName); err != nil {
+		fmt.Println(err)
 		return err
 	}
 
