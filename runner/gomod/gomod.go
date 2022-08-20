035e3e291b3a0eefbde79feb18a6aed6404eca66
tady <ohtukayoshi.yoshi@gmail.com>
2022-08-20 13:52:18 +0900

    add net/http

---
+++ runner/gomod/gomod.go
@@ -0,0 +1,36 @@
+package gomod
+
+import (
+	"os"
+	"os/exec"
+)
+
+func Setup(projectName, packageName string) error {
+	if err := os.Chdir(projectName); err != nil {
+		return err
+	}
+	if err := initialize(packageName); err != nil {
+		return err
+	}
+	if err := tidy(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func initialize(packageName string) error {
+	if err := exec.Command("go", "mod", "init", packageName).Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func tidy() error {
+	if err := exec.Command("go", "mod", "tidy").Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
--- runner/sandbox/sandbox.go
+++ runner/sandbox/sandbox.go
@@ -1,8 +1,11 @@
 package sandbox
 
 import (
+	"fmt"
+
 	"github.com/TadayoshiOtsuka/go-tady/runner/config"
 	"github.com/TadayoshiOtsuka/go-tady/runner/generator"
+	"github.com/TadayoshiOtsuka/go-tady/runner/gomod"
 )
 
 const src = "./templates/sandbox"
@@ -12,14 +15,9 @@ func Create() error {
 	if err := generator.Do(src, projectName); err != nil {
 		return err
 	}
-	// if err := generator.MakeRoot(projectName); err != nil {
-	// 	return err
-	// }
-
-	// if err := generator.Scan(src, projectName); err != nil {
-	// 	fmt.Println(err)
-	// 	return err
-	// }
+	if err := gomod.Setup(projectName, fmt.Sprintf("github.com/TadayoshiOtsuka/%v", projectName)); err != nil {
+		return err
+	}
 
 	return nil
 }
