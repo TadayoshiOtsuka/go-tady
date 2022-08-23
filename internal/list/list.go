41e0d2d01bf9ba6b0b7b1a223d59c0820c63eefe
tady <ohtukayoshi.yoshi@gmail.com>
2022-08-23 20:25:34 +0900

    fix list cmd

--- internal/list/list.go
+++ internal/list/list.go
@@ -3,19 +3,24 @@ package list
 import (
 	"fmt"
 
+	"github.com/TadayoshiOtsuka/go-tady/pkg/errs"
 	"github.com/spf13/viper"
 )
 
 func Exec() error {
-	showPresets()
-
-	return nil
+	return showPresets()
 }
 
-func showPresets() {
+func showPresets() error {
+	err := viper.ReadInConfig()
+	if err != nil {
+		return errs.ErrInitializeNotComplete
+	}
 	presets := viper.GetStringMapString("presets")
 	fmt.Printf("%v preset found\n", len(presets))
-	for v := range presets {
-		fmt.Print(v)
+	for k, v := range presets {
+		fmt.Printf("%v  at: %v\n", k, v)
 	}
+
+	return nil
 }
